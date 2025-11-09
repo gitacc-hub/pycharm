7374bf749e6adb1a338552f146e7937778519e8e
gitacc-hub <github2001acc@gmail.com>
2025-11-09 13:13:54 +0300

    Corrected the note in the test.py file

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 with open("Readme.md","r") as f:
     content=f.read()
     print(content)
-#I will update the code here .Remote from your local repo
+#I updated the code from my local repo.Lets see what happens
